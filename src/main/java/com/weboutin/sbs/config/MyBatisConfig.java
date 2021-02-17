1939f827dda479c72280f86ebe9e52d49119568a
wuzhicong <wuzhicong@aiyunxiao.com>
2021-02-17 23:35:27 +0800

    feat: 删除多余代码

--- src/main/java/com/weboutin/sbs/config/MyBatisConfig.java
+++ src/main/java/com/weboutin/sbs/config/MyBatisConfig.java
@@ -2,15 +2,10 @@ package com.weboutin.sbs.config;
 
 import javax.sql.DataSource;
 
-import com.weboutin.sbs.mappers.UserMapper;
-
-import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.mybatis.spring.SqlSessionFactoryBean;
-import org.mybatis.spring.SqlSessionTemplate;
 
 @Configuration
 public class MyBatisConfig {
@@ -21,16 +16,4 @@ public class MyBatisConfig {
         return DataSourceBuilder.create().build();
     }
 
-    // @Bean
-    // public SqlSessionFactory sqlSessionFactory() throws Exception {
-    //     SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
-    //     factoryBean.setDataSource(dataSource());
-    //     return factoryBean.getObject();
-    // }
-
-    // @Bean
-    // public UserMapper userMapper() throws Exception {
-    //     SqlSessionTemplate sqlSessionTemplate = new SqlSessionTemplate(sqlSessionFactory());
-    //     return sqlSessionTemplate.getMapper(UserMapper.class);
-    // }
 }
